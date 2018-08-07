b0c5d0afb4ecd6878ac6d27c3fa003fc37449165
Lyle Mitchell <ljwmitchell@gmail.com>
2018-08-07 22:28:54 +0100

    adds initial commit with basic gender_variable

---
+++ feed_decision_tree.py
@@ -0,0 +1,3 @@
+from gender_variable import gender_variable
+
+gender_variable("female")
---
+++ gender_variable.py
@@ -0,0 +1,5 @@
+def gender_variable(gender):
+    if(gender == "male"):
+        print ("m_procedure")
+    else:
+        print ("f_procedure")
